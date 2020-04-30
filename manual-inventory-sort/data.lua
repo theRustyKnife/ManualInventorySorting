66466de8b15a26076874295b8adfce4004230487
TheRustyKnife <therustyknife13@gmail.com>
2020-04-30 23:22:42 +0200

    Change consuming to game-only

--- manual-inventory-sort/data.lua
+++ manual-inventory-sort/data.lua
@@ -3,18 +3,18 @@ data:extend{
 		type = 'custom-input',
 		name = 'manual-inventory-sort',
 		key_sequence = 'SHIFT + Q',
-		consuming = 'all',
+		consuming = 'game-only',
 	},
 	{
 		type = 'custom-input',
 		name = 'manual-inventory-sort-opened',
 		key_sequence = 'CONTROL + Q',
-		consuming = 'all',
+		consuming = 'game-only',
 	},
 	{
 		type = 'custom-input',
 		name = 'manual-inventory-auto-sort-toggle',
 		key_sequence = 'SHIFT + Z',
-		consuming = 'all',
+		consuming = 'game-only',
 	},
 }
